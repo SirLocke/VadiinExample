b2eab057e8dc6a1f3ba707c753ab7bde96939c21
SirLocke <vfb.fan4ever.nm@googlemail.com>
2021-06-05 20:33:20 +0200

    Wizard seite 1

--- frontend/src/views/main-view.js
+++ frontend/src/views/main-view.js
@@ -13,18 +13,19 @@ class MainView extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%; height: 100%;">
+<vaadin-vertical-layout style="width: 100%; height: 100%;" theme="test">
  <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct);"></vaadin-horizontal-layout>
  <vaadin-vertical-layout class="content" style="width: 90%; flex-grow: 1; flex-shrink: 1; flex-basis: auto; margin: var(--lumo-space-m); align-self: center; box-shadow: 0px 4px 48px rgba(28,55,90 ,0.16) ;">
   <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
    <div style="width: 100%; background-color: rgba(25, 59, 103, 0.05); flex-grow: 0; padding: var(--lumo-space-m);">
-    Setup Wizard
+     Setup Wizard 
    </div>
   </vaadin-horizontal-layout>
-  <div style="width: 100%; height: 100%;">
-    Div 
-  </div>
-  <img Source="file://D:\Bilder\Walpaper\wolf_silhouette_art_138559_1920x1080.jpg" src="file://D:/Bilder/Walpaper/wolf_silhouette_art_138559_1920x1080.jpg">
+  <img Source="theme://img/schmalz_logo.svg" src="schmalz_logo.svg" style="align-self: center;"> Div </img>
+  <h2 style="align-self: center;">Heading 2 Herzlich Willkommmen beim Setup Wizard von J Schmalz!</h2>
+  <p style="text-align: center; width: 50%; margin-left: auto; margin-right: auto;">Paragraph"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+  <h3 style="align-self: center;">Viel Erfolg</h3>
+  <div style="width: 100%; height: 100%;"></div>
   <vaadin-horizontal-layout theme="spacing" style="width: 100%; align-items: flex-end;">
    <div style="width: 100%; background-color: rgba(25, 59, 103, 0.05); flex-grow: 0; padding: var(--lumo-space-m)">
     <vaadin-progress-bar style="width: 50%; margin-left: auto; margin-right: auto;" max="6" value="0"></vaadin-progress-bar>
